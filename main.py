2cebdeb4d27f000dca765d8e2eb70d0201873956
carnasci <cen.master24@gmail.com>
2025-02-28 18:12:23 -0600

    Day 36 - figured out the percent difference in closing stock prices and alerts in terminal to get news

--- main.py
+++ main.py
@@ -17,7 +17,17 @@ response = requests.get(url=STOCK_ENDPOINT, params=parameters)
 response.raise_for_status()
 print(response.status_code)
 data = response.json()
+print(data)
 daily_data = data["Time Series (Daily)"]
 #Gather list of daily closing prices
 closing_prices = [value["4. close"] for (key, value) in daily_data.items()]
 print(closing_prices)
+yesterday_closing = float(closing_prices[0])
+yesyesterday_closing = float(closing_prices[1])
+positive_difference = abs(yesyesterday_closing - yesterday_closing)
+print(positive_difference)
+percent_diff = (positive_difference/yesyesterday_closing)*100
+print(percent_diff)
+if percent_diff > 5:
+    print("Get News")
+
